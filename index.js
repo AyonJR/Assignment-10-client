c8509f0da3d5dc31b6a3456cbece115945186386
unknown <abdulbaset.ayon@gmail.com>
2024-04-26 10:31:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require ("express") 
+const cors = require ("cors") 
+const app = express() ; 
+const port = process.env.PORT || 5000 ;
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/' , (req , res) => {
+    res.send("Simple Crud is running")
+}) 
+
+app.listen(port , () => {
+    console.log(`simple crud is running on the port ,  ${port}` )
+})
+
+
+
+
+
